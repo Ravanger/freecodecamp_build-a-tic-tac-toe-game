d7f7d810e20226452ed9c22d1fd4e3461a299b06
Boris Rossovsky <boris.rozzy@gmail.com>
2022-12-23 02:08:56 -0500

    Cell component tests

---
+++ src/components/Board/Cell/Cell.test.tsx
@@ -0,0 +1,15 @@
+import Cell from "./Cell"
+import { CellState } from "./Cell.types"
+import { render, screen } from "@testing-library/react"
+
+describe("Cell component", () => {
+  beforeEach(() => {
+    render(<Cell state={CellState.Empty} />)
+  })
+
+  it("should render an empty cell", () => {
+    const cell = screen.getByRole("button")
+    expect(cell).toBeDefined()
+    expect(cell.textContent).toBe("")
+  })
+})
