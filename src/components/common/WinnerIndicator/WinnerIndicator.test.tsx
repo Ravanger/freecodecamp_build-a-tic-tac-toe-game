88cd9c4c4a66c6609c35918b0123b27dc7848d4d
Boris Rossovsky <boris.rozzy@gmail.com>
2022-12-23 02:36:55 -0500

    WinnerIndicator component tests

---
+++ src/components/common/WinnerIndicator/WinnerIndicator.test.tsx
@@ -0,0 +1,19 @@
+import { CellState } from "@components/Board/Cell/Cell.types"
+import { winner } from "@store/game"
+import { render, screen } from "@testing-library/react"
+import WinnerIndicator from "./WinnerIndicator"
+
+describe("WinnerIndicator component", () => {
+  it("should display ? if no winner, by default", () => {
+    render(<WinnerIndicator />)
+    const element = screen.getByText(`Winner: ?`)
+    expect(element).toBeDefined()
+  })
+
+  it("should set the current winner to X and display the correct text", () => {
+    winner.set(CellState.X)
+    render(<WinnerIndicator />)
+    const element = screen.getByText(`Winner: ${CellState.X}`)
+    expect(element).toBeDefined()
+  })
+})
