9d27a1699cd861998494ca9837ab08e0f039afe8
Boris Rossovsky <boris.rozzy@gmail.com>
2022-12-23 02:09:03 -0500

    Button component tests

---
+++ src/components/common/Button/Button.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import Button from "./Button"
+
+describe("Button component", () => {
+  const buttonText = "Click me"
+  const fn = vi.fn()
+
+  beforeEach(() => {
+    render(<Button handleClick={fn}>{buttonText}</Button>)
+  })
+
+  it("should render a button with the correct text content", () => {
+    const button = screen.getByText(buttonText)
+    expect(button).toBeDefined()
+    expect(button.textContent).toBe(buttonText)
+  })
+
+  it("calls the handleClick function when the button is clicked", () => {
+    const button = screen.getByText(buttonText)
+    fireEvent.click(button)
+    expect(fn).toHaveBeenCalledOnce()
+    fireEvent.click(button)
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
+})
