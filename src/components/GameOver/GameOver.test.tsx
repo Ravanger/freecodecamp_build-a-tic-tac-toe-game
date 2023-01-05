7c4dd1ba318438776fbbacc1f9ebd8ccd523d4f6
Boris Rossovsky <boris.rozzy@gmail.com>
2023-01-04 21:38:08 -0500

    GameOver component test

---
+++ src/components/GameOver/GameOver.test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import GameOver from "./GameOver"
+import { gameState } from "@store/game"
+import { GameState } from "@components/Game/Game.types"
+
+describe("GameOver component", () => {
+  beforeEach(() => {
+    gameState.set(GameState.Playing)
+  })
+
+  it("renders correct text if a tie", () => {
+    render(<GameOver isTie={true} />)
+    const element = screen.getByText("It's a tie!")
+    expect(element).toBeDefined()
+  })
+
+  it("renders 'winner' text when the game is not a tie", () => {
+    render(<GameOver isTie={false} />)
+    const element = screen.getByText(/winner/i, { exact: false })
+    expect(element).toBeDefined()
+  })
+
+  it("renders the 'Restart Game' button and resets the game when clicked", () => {
+    render(<GameOver isTie={false} />)
+    const button = screen.getByText("Restart Game")
+    expect(button).toBeDefined()
+
+    expect(gameState.get()).toBe(GameState.Playing)
+    fireEvent.click(button)
+    expect(gameState.get()).toBe(GameState.Menu)
+  })
+})
